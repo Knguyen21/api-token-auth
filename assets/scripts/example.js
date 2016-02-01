8ee46c7d124957bad548e72aa77542a4239a93b2
khanhnhat <nguyen.k21@gmail.com>
2016-02-01 13:31:46 -0500

    Lesson complete

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,17 +1,65 @@
 'use strict';
 
+const myApp = {
+  baseURL: 'http://10.13.105.91:3000',
+}
+
 $(document).ready(() => {
   $('#sign-up').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(e.target);
     $.ajax({
-      // url: 'http://localhost:3000/sign-up',
-      url: 'http://httpbin.org/post',
+      url: myApp.baseURL + '/sign-up',
+      // url: 'http://httpbin.org/post',
       method: 'POST',
       contentType: false,
       processData: false,
       data: formData,
     }).done(function(data) {
+      console.log(data);
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  });
+
+  $('#sign-in').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.baseURL + '/sign-in',
+      // url: 'http://httpbin.org/post',
+      method: 'POST',
+      contentType: false,
+      processData: false,
+      data: formData,
+    }).done(function(data) {
+      myApp.user = data.user;
+      console.log(data);
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  });
+
+  $('#change-password').on('submit', function(e) {
+    e.preventDefault();
+    if (!myApp.user) {
+      console.error('Wrong!');
+      return;
+    }
+
+    var formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.baseURL + '/change-password/' + myApp.user.id,
+      // url: 'http://httpbin.org/post',
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      },
+      contentType: false,
+      processData: false,
+      data: formData,
+    }).done(function(data) {
+      console.log('successful data');
       console.log(data);
     }).fail(function(jqxhr) {
       console.error(jqxhr);
